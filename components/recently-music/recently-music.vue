35a1a291fcabf429ff307a4af0ea608d711d9207
Ricardo <532569829@qq.com>
2020-08-06 16:27:11 +0800

    update .gitignore

--- components/recently-music/recently-music.vue
+++ components/recently-music/recently-music.vue
@@ -12,6 +12,7 @@
 
 <script>
 	export default {
+		name: 'recently-music',
 		props: {
 			
 		},
--- pages/player/player.vue
+++ pages/player/player.vue
@@ -135,7 +135,14 @@
 		height: 100%;
 		background-color: #0e0b1f;
 	}
-	
+	//定义动画
+	@keyframes turn{
+	  0%{-webkit-transform:rotate(0deg);}
+	  25%{-webkit-transform:rotate(90deg);}
+	  50%{-webkit-transform:rotate(180deg);}
+	  75%{-webkit-transform:rotate(270deg);}
+	  100%{-webkit-transform:rotate(360deg);}
+	}
 	.img-box {
 		display: flex;
 		justify-content: space-around;
@@ -148,6 +155,7 @@
 			background-color: #999999;
 			border-radius: 50%;
 			margin: 300rpx auto 44rpx;
+			animation:turn 5s linear infinite;
 		}
 		text {
 			font-family: Helvetica;
