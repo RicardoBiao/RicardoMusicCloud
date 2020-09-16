a755e29df6d2246ef5a74e94fb9deba423ff95e2
Ricardo <532569829@qq.com>
2020-09-16 11:52:03 +0800

    Merge branch 'develop' of https://github.com/RicardoBiao/RicardoMusicCloud into develop
    # Conflicts:
    #	main.js

--- main.js
+++ main.js
@@ -4,9 +4,6 @@ import api from './utils/api'
 import store from '@/vuex/store'
 // import VueLazyload from 'vue-lazyload'
 // Vue.use(VueLazyload)
-=======
-import store from '@/vuex/store'
->>>>>>> aeea84f02d10b3b0ebec6cd3459a6b553d9cc723
 
 Vue.prototype.$api = api
 Vue.prototype.$store = store
